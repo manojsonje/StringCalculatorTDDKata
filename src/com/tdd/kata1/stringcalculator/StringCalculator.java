d28b75946eef5b97972b8c92bc5686db6d6c8407
Manoj Sonje <manojsonje1595@gmail.com>
2024-09-25 22:28:59 +0530

    refactor2 : refactored the code for test2
    refactor2 : refactored the code for test2

--- src/com/tdd/kata1/stringcalculator/StringCalculator.java
+++ src/com/tdd/kata1/stringcalculator/StringCalculator.java
@@ -15,17 +15,15 @@ public class StringCalculator {
 	}
 
 	public int Add(String numbers) {
-		if (numbers.isEmpty()) {
-			return 0;
-		}
+        if (numbers.isEmpty()) {
+            return 0;
+        }
 
-		String[] nums = numbers.split(",");
-		if (nums.length == 1) {
-			return Integer.parseInt(nums[0]);
-		} else if (nums.length == 2){
-			return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
-		} else {
-			return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]) + Integer.parseInt(nums[2]) ;
-		}
-	}
+        String[] nums = numbers.split(",");
+        int sum = 0;
+        for (String num : nums) {
+            sum += Integer.parseInt(num);
+        }
+        return sum;
+    }
 }
