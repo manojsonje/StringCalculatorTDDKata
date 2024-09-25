73b6420b8b2e499d79d6cb55da232c3d8d6e24e2
Manoj Sonje <manojsonje1595@gmail.com>
2024-09-25 22:15:23 +0530

    code1.3 : code to pass test1.3
    code1.3 : code to pass test1.3 failing testcase for two numbers

--- src/com/tdd/kata1/stringcalculator/StringCalculator.java
+++ src/com/tdd/kata1/stringcalculator/StringCalculator.java
@@ -13,11 +13,17 @@ public class StringCalculator {
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	public int Add(String numbers) {
-        if (numbers.isEmpty()) {
-            return 0;
-        }
-        return Integer.parseInt(numbers);
-    }
+		if (numbers.isEmpty()) {
+			return 0;
+		}
+
+		String[] nums = numbers.split(",");
+		if (nums.length == 1) {
+			return Integer.parseInt(nums[0]);
+		} else {
+			return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
+		}
+	}
 }
