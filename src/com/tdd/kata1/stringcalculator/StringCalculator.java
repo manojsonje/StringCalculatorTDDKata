e340c8b9327d7465a40a286ea9bdacbb22247001
Manoj Sonje <manojsonje1595@gmail.com>
2024-09-26 22:33:02 +0530

    code5 : code to pass failing test5.1
    code5 : code to pass failing test5.1 multiple negative Number throwing exception

--- src/com/tdd/kata1/stringcalculator/StringCalculator.java
+++ src/com/tdd/kata1/stringcalculator/StringCalculator.java
@@ -1,5 +1,9 @@
 package com.tdd.kata1.stringcalculator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author Manoj
  *
@@ -15,22 +19,33 @@ public class StringCalculator {
 	}
 
 	public int Add(String numbers) {
-		if (numbers.isEmpty()) {
-			return 0;
-		}
+	    if (numbers.isEmpty()) {
+	        return 0;
+	    }
 
-		String delimiter = ",|\n"; // Default delimiters are comma and newline
-		if (numbers.startsWith("//")) {
-			int delimiterIndex = numbers.indexOf("\n");
-			delimiter = numbers.substring(2, delimiterIndex);
-			numbers = numbers.substring(delimiterIndex + 1);
-		}
+	    String delimiter = ",|\n";
+	    if (numbers.startsWith("//")) {
+	        int delimiterIndex = numbers.indexOf("\n");
+	        delimiter = numbers.substring(2, delimiterIndex);
+	        numbers = numbers.substring(delimiterIndex + 1);
+	    }
 
-		String[] nums = numbers.split(delimiter);
-		int sum = 0;
-		for (String num : nums) {
-			sum += Integer.parseInt(num);
-		}
-		return sum;
+	    String[] nums = numbers.split(delimiter);
+	    int sum = 0;
+	    List<Integer> negatives = new ArrayList<>();
+
+	    for (String num : nums) {
+	        int n = Integer.parseInt(num);
+	        if (n < 0) {
+	            negatives.add(n);
+	        }
+	        sum += n;
+	    }
+
+	    if (!negatives.isEmpty()) {
+	        throw new RuntimeException("negative numbers not allowed " + negatives.stream().map(String::valueOf).collect(Collectors.joining(",")));
+	    }
+
+	    return sum;
 	}
 }
