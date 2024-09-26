10bd06330adb13756b5ab1ac818b6313866f68e7
Manoj Sonje <manojsonje1595@gmail.com>
2024-09-26 21:43:19 +0530

    code3 : code to pass failing test3
    code3 : code to pass failing test3 for numbers containing newline

--- src/com/tdd/kata1/stringcalculator/StringCalculator.java
+++ src/com/tdd/kata1/stringcalculator/StringCalculator.java
@@ -15,15 +15,15 @@ public class StringCalculator {
 	}
 
 	public int Add(String numbers) {
-        if (numbers.isEmpty()) {
-            return 0;
-        }
+		if (numbers.isEmpty()) {
+			return 0;
+		}
 
-        String[] nums = numbers.split(",");
-        int sum = 0;
-        for (String num : nums) {
-            sum += Integer.parseInt(num);
-        }
-        return sum;
-    }
+		String[] nums = numbers.split("[,\n]");
+		int sum = 0;
+		for (String num : nums) {
+			sum += Integer.parseInt(num);
+		}
+		return sum;
+	}
 }
